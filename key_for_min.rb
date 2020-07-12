7ef110d95cac590de03562c143d06f545781d85d
Jacob Viti <thevitij64@gmail.com>
2020-07-12 03:58:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
 #if lowestNumber isn't is lower than the lowest number given, enter more 9s :^)
   lowestNumber = 999999999999999999999999999999999999999999999999999999999999999
+  lowestName = nil
   name_hash.collect do |name, number|
     if number < lowestNumber
       lowestNumber = number
